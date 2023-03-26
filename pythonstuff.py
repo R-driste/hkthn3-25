d48128545d7b34dc7c0db4fc739b9c774d388ff7
Cooper579 <552687@my.cuhsd.org>
2023-03-26 13:18:08 -0700

    dristi wanted this

---
+++ pythonstuff.py
@@ -0,0 +1,6 @@
+#make a function that takes a photo and makes 300 white pixels below it
+#then make a caption in teh 300 pixels
+from PIL import Image
+
+image1 = Image.open("C:\Users\BRHS-PLTW-09\Documents\Hack\3d39586ffcd9894e4c4eed50cd93e094-removebg-preview.png")
+image1.show()
