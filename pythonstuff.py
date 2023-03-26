6255be801bb58393bc931d5798f972030619cd56
lord-turner <559553@my.cuhsd.org>
2023-03-26 14:39:32 -0700

    william wuz here

--- pythonstuff.py
+++ pythonstuff.py
@@ -1,6 +1,21 @@
 #make a function that takes a photo and makes 300 white pixels below it
 #then make a caption in teh 300 pixels
 from PIL import Image
+import os
 
-image1 = Image.open("C:\Users\BRHS-PLTW-09\Documents\Hack\3d39586ffcd9894e4c4eed50cd93e094-removebg-preview.png")
+image1 = Image.open(r"C:\Users\BRHS-PLTW-09\Documents\Hack\ex1.png")
 image1.show()
+
+def add_margin(pil_img, bottom,color):
+    width, height = pil_img.size
+    new_height = height + bottom
+    result = Image.new(pil_img.mode, (width, new_height), color)
+    result.paste(pil_img)
+    return result
+
+
+
+
+im_new = add_margin(image1, 100,"white")
+im_new.save(r"C:\Users\BRHS-PLTW-09\Documents\Hack\ex1.png", quality=95)
+im_new.show()
